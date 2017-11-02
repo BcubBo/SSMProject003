0de33b15a08afb879096bf4bd879e05dd3215b3b
bcubbo <fatalitywolf@outlook.com>
2017-11-03 07:51:32 +0800

    暂时搁置

--- src/com/bcubbo/service/Impl/UserSerivceImpl.java
+++ src/com/bcubbo/service/Impl/UserSerivceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bcubbo.dao.UserMapper;
@@ -12,7 +13,7 @@ import com.bcubbo.service.UserService;
 @Service
 public class UserSerivceImpl implements UserService {
 	
-	@Resource
+	@Autowired
 	private UserMapper userMapper;
 	
 	@Override
